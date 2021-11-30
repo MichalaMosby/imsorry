4edd74549afb4733fe7bb5b5bfd6c728dc837551
MichalaMosby <91148182+MichalaMosby@users.noreply.github.com>
2021-11-30 12:06:50 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var rose=0;
+
+//The setup function only happens once
+function setup() {
+	createCanvas(600, 500); //create a 500px X 500px canvas
+background(17, 17, 17  );
+}
+
+//The draw function happens over and over again
+function draw() {
+   //an RGB color for the canvas' background
+  //circle
+ stroke (255, 255, 255)
+ fill(255, 255, 255)
+textSize(10)
+textFont("Allura",40,40)
+textStyle(ITALIC)
+text("I'm Sorry I was such a bad sister",50,300)
+}
+
